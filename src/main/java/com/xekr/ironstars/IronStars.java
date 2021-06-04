b923661eda3d7807ea48e6276c30eec0099ab6ec
LucunJi <lucunjif@gmail.com>
2021-06-03 20:20:40 -0500

    format en_us.json

--- src/main/java/com/xekr/ironstars/IronStars.java
+++ src/main/java/com/xekr/ironstars/IronStars.java
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("iron-stars")
+@Mod(IronStars.ID)
 public class IronStars {
 
     public static final String ID = "iron-stars";
