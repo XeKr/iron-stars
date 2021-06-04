a4fc8c2f2d990972f36c9a6ace59be1c8d2e9384
LucunJi <lucunjif@gmail.com>
2021-06-03 20:09:55 -0500

    revert change in modid

--- src/main/java/com/xekr/ironstars/IronStars.java
+++ src/main/java/com/xekr/ironstars/IronStars.java
@@ -19,10 +19,10 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("iron_stars")
+@Mod("iron-stars")
 public class IronStars {
 
-    public static final String ID = "iron_stars";
+    public static final String ID = "iron-stars";
     public static final String NAME = "Iron Stars";
     public static final String VERSION = "1.0";
     // Directly reference a log4j logger.
